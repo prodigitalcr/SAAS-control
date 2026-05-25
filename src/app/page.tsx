e5c5a09655e282f34be9eebcbcfbc4c28373b662
prodigital <norercarlop04@gmail.com>
2026-05-25 12:32:55 -0600

    Prueba de Hola Mundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-// Entry point → Sign Up / Login Screen
-import LoginPage from './sign-up-login-screen/page';
-
 export default function RootPage() {
-  return <LoginPage />;
+  return <h1>Hola Mundo desde la raíz. ¡Vercel sí funciona!</h1>;
 }
